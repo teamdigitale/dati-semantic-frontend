2ac06b6d9c349f031a41515e51bc8dc433cb077d
Ginette Vellera <ginettv@thoughtworks.com>
2021-10-25 18:11:16 +0530

    54 - fixing all the super linter issues

--- scripts/super-linter.sh
+++ scripts/super-linter.sh
@@ -1,2 +1,2 @@
 docker pull github/super-linter:latest
-docker run -e RUN_LOCAL=true -e VALIDATE_BASH=false -e VALIDATE_MARKDOWN=false -e FILTER_REGEX_INCLUDE='.*(src|\.github)/.*' -e FILTER_REGEX_EXCLUDE='.*.(min.css)' -v ~/project/digitale/dati-semantic-frontend:/tmp/lint github/super-linter
+docker run -e RUN_LOCAL=true -e VALIDATE_BASH=false -e VALIDATE_MARKDOWN=false -e JAVASCRIPT_ES_CONFIG_FILE='../../.eslintrc.json' -e JAVASCRIPT_DEFAULT_STYLE='prettier' -e CSS_FILE_NAME='../../.stylelintrc.json' -e FILTER_REGEX_INCLUDE='.*(src|\.github)/.*' -e FILTER_REGEX_EXCLUDE='.*.(min.css)' -v ~/project/digitale/dati-semantic-frontend:/tmp/lint github/super-linter
