11383d986b3e7319d59497950e59d779b7a6ea1d
Rony Cesana <rony.cesana@thoughtworks.com>
2021-11-08 16:06:12 +0100

    60 - Fixed test name

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders Team Digitale title", () => {
   render(<App />);
   const linkElement = screen.getByText("Team Digitale");
   expect(linkElement).toBeInTheDocument();
