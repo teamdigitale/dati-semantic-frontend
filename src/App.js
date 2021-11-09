25b8c0d06e845925474f97a805a7740f7d25b3f5
Rony Cesana <rony.cesana@thoughtworks.com>
2021-11-09 13:43:29 +0100

    60 - Add Main component for page body

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
 
 function App() {
-  return <Header />;
+  return (
+    <div id="page-front">
+      <Header />
+      <Main />
+    </div>
+  );
 }
 
 export default App;
