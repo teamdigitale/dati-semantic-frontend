938e9190216db8afa39a05bfae2e891db3b6bc73
Rony Cesana <rony.cesana@thoughtworks.com>
2021-11-09 16:14:32 +0100

    60 - Show categories in main body

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,9 +1,33 @@
 import React from "react";
+import { getCategories } from "../../assets/data/categories";
+import "./Categories.css";
+
+const categoryData = getCategories();
 
 const Categories = () => (
   <div data-testid="Categories">
-    <div className="row">
+    <div className="row p-3">
       <h2>naviga i dati per categoria tematica</h2>
+    </div>
+    <div className="row" role="list">
+      {categoryData.map((category) => (
+        <div key={category.key} className="col-3">
+          <div
+            className="p-2 m-1 align-middle border shadow-sm"
+            role="listitem"
+          >
+            <p className="font-weight-bolder clearfix">
+              {" "}
+              <img
+                className={`${category.key.toLowerCase()}-category-icon category-icon float-left`}
+                title={category.label}
+                alt={category.key}
+              />
+              {category.label}
+            </p>
+          </div>
+        </div>
+      ))}
     </div>
   </div>
 );
--- src/components/Categories/Categories.test.js
+++ src/components/Categories/Categories.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import Categories from "./Categories";
+import { getCategories } from "../../assets/data/categories";
 
 describe("<Categories />", () => {
   test("it should mount", () => {
@@ -10,5 +11,13 @@ describe("<Categories />", () => {
     const categories = screen.getByText("naviga i dati per categoria tematica");
 
     expect(categories).toBeInTheDocument();
+  });
+
+  test("it should contain list item per category", () => {
+    render(<Categories />);
+
+    const categories = screen.getAllByRole("listitem");
+
+    expect(categories.length).toBe(getCategories().length);
   });
 });
