8d3b62d2ad08546196e944bc215723b64229d262
Rony Cesana <rony.cesana@thoughtworks.com>
2021-11-13 13:57:07 +0100

    60 - Search service to search across different asset types

--- src/services/searchService.js
+++ src/services/searchService.js
@@ -3,10 +3,16 @@
  * @returns {Promise<unknown>}
  */
 import { getVocabularyMetadata } from "../assets/data/vocabularyMetadata";
+import { getOntologyMetadata } from "../assets/data/ontologyMetadata";
+import { AT_ONTOLOGY, AT_VOCABULARY } from "./dataConstants";
 
-export async function search({ pattern = "" } = {}) {
+export async function search({ pattern = "", type = "" } = {}) {
   return new Promise((resolve) => {
+    let vocabs =
+      type === AT_VOCABULARY ? getVocabularyMetadata({ pattern }) : [];
+    let ontos = type === AT_ONTOLOGY ? getOntologyMetadata({ pattern }) : [];
+
     const timeout = Math.random() * 200;
-    setTimeout(() => resolve(getVocabularyMetadata({ pattern })), timeout);
+    setTimeout(() => resolve(vocabs.concat(ontos)), timeout);
   });
 }
--- src/services/searchService.test.js
+++ src/services/searchService.test.js
@@ -1,35 +1,49 @@
 import { search } from "./searchService";
+import { AT_ONTOLOGY, AT_VOCABULARY } from "./dataConstants";
 
 describe("Search service", () => {
-  test("should return all vocabularies", async () => {
-    const items = await search();
+  test("should return all vocabularies, capped at 20", async () => {
+    const items = await search({ type: AT_VOCABULARY });
 
-    expect(items.length).toBe(38);
+    expect(items.length).toBe(20);
   });
 
   test("should find a vocabulary for 'Ripartizioni', looking for keyword", async () => {
-    const items = await search({ pattern: "Ripartizioni" });
+    const items = await search({
+      pattern: "Ripartizioni",
+      type: AT_VOCABULARY,
+    });
 
     expect(items.length).toBe(1);
-    expect(items[0].dataset).toBe(
+    expect(items[0].uri).toBe(
       "https://w3id.org/italia/controlled-vocabulary/territorial-classifications/geographical-distribution"
     );
   });
 
   test("should find a vocabulary for 'ripartizioni', making keyword lowercase", async () => {
-    const items = await search({ pattern: "ripartizioni" });
+    const items = await search({
+      pattern: "ripartizioni",
+      type: AT_VOCABULARY,
+    });
 
     expect(items.length).toBe(1);
-    expect(items[0].dataset).toBe(
+    expect(items[0].uri).toBe(
       "https://w3id.org/italia/controlled-vocabulary/territorial-classifications/geographical-distribution"
     );
   });
+
   test("should find a vocabulary for 'generazione', searching in description", async () => {
-    const items = await search({ pattern: "generazione" });
+    const items = await search({ pattern: "generazione", type: AT_VOCABULARY });
 
     expect(items.length).toBe(1);
-    expect(items[0].dataset).toBe(
+    expect(items[0].uri).toBe(
       "https://w3id.org/italia/controlled-vocabulary/territorial-classifications/cities"
     );
+  });
+
+  test("should find all ontologies, capped at 20", async () => {
+    const items = await search({ type: AT_ONTOLOGY });
+
+    expect(items.length).toBe(16);
   });
 });
