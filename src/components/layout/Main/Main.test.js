9d62315868055cfc182527b21aab3ead0211bae0
Rony Cesana <rony.cesana@thoughtworks.com>
2021-11-14 14:36:11 +0100

    60 - Mock children for the Main component test

--- src/components/layout/Main/Main.test.js
+++ src/components/layout/Main/Main.test.js
@@ -3,8 +3,27 @@ import { screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import Main from "./Main";
 import { renderWithRoute } from "../../../services/testUtils";
+import NotFound from "../NotFound/NotFound";
+import Categories from "../../Categories/Categories";
+
+jest.mock("../../Categories/Categories", () => ({
+  __esModule: true,
+  default: jest.fn(),
+}));
+
+jest.mock("../NotFound/NotFound", () => ({
+  __esModule: true,
+  default: jest.fn(),
+}));
 
 describe("<Main />", () => {
+  beforeEach(() => {
+    Categories.mockClear();
+    Categories.mockReturnValue(<div>Categories</div>);
+    NotFound.mockClear();
+    NotFound.mockReturnValue(<p>Not found</p>);
+  });
+
   test("it should mount", () => {
     renderWithRoute(<Main />);
 
@@ -13,11 +32,17 @@ describe("<Main />", () => {
     expect(main).toBeInTheDocument();
   });
 
+  test("it should display categories by default", () => {
+    renderWithRoute(<Main />);
+
+    expect(Categories).toHaveBeenCalledTimes(1);
+    expect(NotFound).not.toHaveBeenCalled();
+  });
+
   test("it should display an error for a wrong URL", () => {
     renderWithRoute(<Main />, "/not-existing");
 
-    const main = screen.getByRole("main");
-
-    expect(main).toContainHTML("non è disponibile");
+    expect(Categories).not.toHaveBeenCalled();
+    expect(NotFound).toHaveBeenCalledTimes(1);
   });
 });
