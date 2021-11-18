3764db2847b13a7430c503952fe1158636d358fb
Ginette Vellera <ginettv@thoughtworks.com>
2021-11-18 14:33:17 +0530

    Tech - including bootstrap italia js
    (cherry picked from commit 00720b1cb6c5aa1396658800a75e414bb9a8aeeb)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import "./App.css";
 import Header from "./components/layout/Header/Header";
 import Main from "./components/layout/Main/Main";
 import { BrowserRouter } from "react-router-dom";
+import "bootstrap-italia/dist/css/bootstrap-italia.min.css";
+import "./global-bootstrap-italia";
+import "owl.carousel";
+import "bootstrap-italia";
 
 function App() {
   return (
