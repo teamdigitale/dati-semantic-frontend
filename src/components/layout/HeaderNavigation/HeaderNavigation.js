1fa673435065bfe5bed2dbec541fe5092e049f7a
Rony Cesana <rony.cesana@thoughtworks.com>
2021-11-18 15:14:02 +0100

    UX - Main menu removal

--- src/components/layout/HeaderNavigation/HeaderNavigation.js
+++ src/components/layout/HeaderNavigation/HeaderNavigation.js
@@ -1,11 +1,10 @@
 import React from "react";
 import HeaderMainTitle from "../HeaderMainTitle/HeaderMainTitle";
-import HeaderMainMenu from "../HeaderMainMenu/HeaderMainMenu";
 
 const HeaderNavigation = () => (
   <div className="it-nav-wrapper">
     <HeaderMainTitle />
-    <HeaderMainMenu />
+    {/*<HeaderMainMenu />*/}
   </div>
 );
 
