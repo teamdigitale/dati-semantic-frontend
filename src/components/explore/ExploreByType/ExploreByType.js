80184d263d5c8a996c7843c9946e55e8e7aaddb4
Srinivasan Sekar <srinivasan.sekar1990@gmail.com>
2021-12-14 19:18:15 +0530

    align explore by type buttons
    Co-authored-by: Ankur-Marwaha<amarwaha0@gmail.com>

--- src/components/explore/ExploreByType/ExploreByType.js
+++ src/components/explore/ExploreByType/ExploreByType.js
@@ -1,10 +1,8 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { routes } from "../../../services/routes";
-import CategoryIcon from "../../common/CategoryIcon/CategoryIcon";
 import { AT_TO_LABEL } from "../../../services/dataConstants";
 import ExploreGrid from "../ExploreGrid/ExploreGrid";
-import { getCategories } from "../../../assets/data/categories";
 import ExploreSection from "../ExploreSection/ExploreSection";
 
 const ExploreByType = () => {
@@ -14,18 +12,18 @@ const ExploreByType = () => {
     navigate(routes.search({ types: [type] }));
   };
 
-  const category = getCategories()[2];
-
   const typeCells = AT_TO_LABEL.map((ttl) => ({
     key: ttl.type,
-    icon: <CategoryIcon category={category} className="mt-5" />,
     label: ttl.label,
     onClick: () => searchFor(ttl.type),
   }));
 
   return (
     <ExploreSection title="Esplora gli strumenti semantici per tipo">
-      <ExploreGrid cells={typeCells} />
+      <ExploreGrid
+        cells={typeCells}
+        optionalClass={"btn-primary text-white "}
+      />
     </ExploreSection>
   );
 };
--- src/components/explore/ExploreGrid/ExploreGrid.js
+++ src/components/explore/ExploreGrid/ExploreGrid.js
@@ -3,7 +3,7 @@ import { arrayOf, element, func, shape, string } from "prop-types";
 import styles from "./ExploreGrid.module.css";
 import { chunk } from "../../../services/arrayUtils";
 
-const ExploreGrid = ({ cells }) => (
+const ExploreGrid = ({ cells, optionalClass }) => (
   <div data-testid="ExploreGrid" role="list">
     {chunk(cells, 3).map((c, index) => (
       <div key={"gridRow" + index} className="row" role="row">
@@ -14,7 +14,9 @@ const ExploreGrid = ({ cells }) => (
             onClick={cell.onClick}
             role="cell"
           >
-            <div className="p-2 mx-2 mt-3 mb-2 align-middle shadow-lg">
+            <div
+              className={`p-2 mx-2 mt-3 mb-2 align-middle shadow-lg ${optionalClass}`}
+            >
               <div>
                 <div className="mx-auto w-75">{cell.icon}</div>
                 <p className="font-weight-bolder clearfix m-3 text-lg-center">
@@ -34,10 +36,11 @@ ExploreGrid.propTypes = {
     shape({
       key: string.isRequired,
       onClick: func,
-      icon: element.isRequired,
+      icon: element,
       label: string.isRequired,
     })
   ),
+  optionalClass: string,
 };
 
 ExploreGrid.defaultProps = {};
