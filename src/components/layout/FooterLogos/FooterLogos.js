18269e23f2d053dfe9aede6af167dc5ba4f801ff
Simone Carriero <simone.carriero@thoughtworks.com>
2022-06-29 14:12:20 +0200

    #UI Fix responsiveness footer logos

--- src/components/layout/FooterLogos/FooterLogos.js
+++ src/components/layout/FooterLogos/FooterLogos.js
@@ -6,28 +6,23 @@ import "./FooterLogos.css";
 const FooterLogos = () => {
   return (
     <div className="container">
-      <div className="row">
-        <div className="col-12">
-          <div className="footer_logoteam">
-            <div className="footer_logo_img">
-              <a
-                aria-label="Vai al sito del Dipartimento per la trasformazione digitale"
-                href="https://innovazione.gov.it/dipartimento/"
-              >
-                <Logo
-                  className="img-fluid img-logo-header-normal d-inline-block align-top p-2 rounded"
-                  alt=""
-                  title="Dipartimento per la trasformazione digitale"
-                />
-              </a>
-              <a
-                aria-label="Vai al sito dell'ISTAT"
-                href="https://www.istat.it/"
-              >
-                <IstatLogo className="istat-logo" alt="" title="Istat" />
-              </a>
-            </div>
-          </div>
+      <div className="row pl-4 footer_logoteam">
+        <div className="footer_logo_img">
+          <a
+            aria-label="Vai al sito del Dipartimento per la trasformazione digitale"
+            href="https://innovazione.gov.it/dipartimento/"
+          >
+            <Logo
+              className="img-fluid img-logo-header-normal d-inline-block align-top p-2 rounded"
+              alt=""
+              title="Dipartimento per la trasformazione digitale"
+            />
+          </a>
+        </div>
+        <div className="footer_logo_img">
+          <a aria-label="Vai al sito dell'ISTAT" href="https://www.istat.it/">
+            <IstatLogo className="istat-logo" alt="" title="Istat" />
+          </a>
         </div>
       </div>
     </div>
