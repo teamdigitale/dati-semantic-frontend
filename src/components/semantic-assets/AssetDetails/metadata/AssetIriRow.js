99e214b6f18fec2b78e1f0bce1c384ca6cb78f8a
BelishtaArjol <belishtaarjol@gmail.com>
2023-07-17 17:45:58 +0200

    Fix lint

--- src/components/semantic-assets/AssetDetails/metadata/AssetIriRow.js
+++ src/components/semantic-assets/AssetDetails/metadata/AssetIriRow.js
@@ -4,45 +4,46 @@ import React from "react";
 import rowStyle from "../metadata/MetadataRow.module.css";
 
 const AssetIriRow = (props) => {
-  return (
-    <div className="row" data-testid="asset-iri-row">
-      <div className={"col-3 strong "}>
-        <h3 className={"strong " + rowStyle.propertyName}>URI</h3>
-      </div>
-      <div className="col-8">
-        <div className={"text-monospace " + rowStyle.propertyLink}>
-          <a
-            href={props.assetIri}
-            target="_blank"
-            rel="noreferrer"
-            className={rowStyle.assetLink}
-          >
-            {props.assetIri}
-          </a>
-        </div>
-      </div>
-      <div className="col-1">
-        <a
-          aria-label="Vai all'URI dell'asset (si apre in un'altra scheda)"
-          className="btn btn-sm pt-0"
-          href={props.assetIri}
-          target="_blank"
-          rel="noreferrer"
-        >
-          <svg
-            className="icon icon-primary icon-sm"
-            data-testid="external-link-icon"
-          >
-            <use href={sprite + "#it-external-link"} />
-          </svg>
-        </a>
-      </div>
-    </div>
-  );
+  return (
+    <div className="row" data-testid="asset-iri-row">
+      <div className={" ol-3 strong "}>
+        <h3 className={"strong " + rowStyle.propertyName}>URI</h3>
+      </div>
+      <div className="col-8">
+        <div className={"text-monospace " + rowStyle.propertyLink}>
+          <a
+            href={props.assetIri}
+            target="_blank"
+            rel="noreferrer"
+            className={rowStyle.assetLink}
+          >
+            {props.assetIri}
+          </a>
+        </div>
+      </div>
+      <div className="col-1">
+        <a
+          aria-label="Vai all'URI dell'asset (si apre in un'altra scheda)"
+          className="btn btn-sm pt-0"
+          href={props.assetIri}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <svg
+            className="icon icon-primary icon-sm"
+            data-testid="external-link-icon"
+          >
+            <use href={sprite + "#it-external-link"} />
+          </svg>
+        </a>
+      </div>
+    </div>
+  );
 };
 
 AssetIriRow.propTypes = {
-  assetIri: PropTypes.string.isRequired,
+  assetIri: PropTypes.string.isRequired,
 };
 
 export default AssetIriRow;
+IriRow;
