996f66909de06182736cd97e3bda2a90f90cbe13
BelishtaArjol <belishtaarjol@gmail.com>
2023-07-25 12:20:18 +0200

    Note Legali

--- src/components/static-content/legal-notices/legalNotices.js
+++ src/components/static-content/legal-notices/legalNotices.js
@@ -1,8 +1,9 @@
+/* eslint-disable react/no-unescaped-entities */
 import React, { useEffect } from "react";
 import getAlertMessage from "../../../services/alertService";
 import BREADCRUMBS from "../../../services/BreadCrumbsConst";
 import BreadCrumbs from "../../common/BreadCrumbs/BreadCrumbs";
-import ContentParagraph from "../../common/ContentParagraph/ContentParagraph";
+import Anchor from "../../common/Anchor/Anchor";
 import "../privacy-policy/PrivacyPolicyPage/PrivacyPolicyPage.css";
 export const LeaglNotices = () => {
   useEffect(() => {
@@ -31,23 +32,87 @@ export const LeaglNotices = () => {
       <div data-testid="legalNoticesBody" className="policy-body">
         <div className="container-fluid schemaPadding">
           <div className="col-lg-6 pt-5">
-            <div className="text-center text-sm-left pb-4">
-              <h1>Note Legali</h1>
+            <div className="text-center text-sm-left pb-4 display-1">
+              Note Legali
             </div>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been standard dummy text ever since the
-              1500s, when an unknown printer took a galley of type and scrambled
-              it to make a type specimen book. It has survived not only five
-              centuries, but also the leap into electronic typesetting,
-              remaining essentially unchanged. It was popularised in the 1960s
-              with the release of Letraset sheets containing Lorem Ipsum
-              passages, and more recently with desktop publishing software like
-              Aldus PageMaker including versions of Lorem Ipsum.
+            <p style={{ fontSize: 20 }}>
+              Il catalogo nazionale della semantica dei dati (anche conosciuto
+              come National Data Catalog - abbreviato NDC) e raggiungibile da
+              www.schema.gov.it, è un progetto di titolarità del Dipartimento
+              per la Trasformazione Digitale – Presidenza Del Consiglio dei
+              ministri. Ha il compito di armonizzare e condividere la semantica
+              dei dati utile a garantire l'interoperabilità semantica
+              nell'interscambio e fruizione di dati, in particolare tramite
+              interfacce per la programmazione di applicazioni (API), come
+              quelle reperibili nel catalogo della Piattaforma Digitale
+              Nazionale Dati (PDND). Il catalogo, oltre a pubblicare i contenuti
+              definiti a livello nazionale, raccoglie - tramite estrazione
+              automatica (web harvesting) - le risorse semantiche già pubblicate
+              dalle singole pubbliche amministrazioni nei propri repository di
+              cui gli stessi enti sono gli unici titolari, mantenendo così la
+              responsabilità in merito al contenuto.
             </p>
-            <ContentParagraph title="Test">
-              <p>test paragrafo 1</p>
-            </ContentParagraph>
+            <p style={{ fontSize: 20 }}>
+              Nell’ambito dei progetti del Piano Nazionale di Ripresa e
+              Resilienza (PNNR), il Dipartimento per la trasformazione digitale,
+              ha individuato Istat quale ente attuatore del progetto NDC per la
+              realizzazione, gestione e manutenzione del catalogo, mentre
+              l’accordo prevede il coinvolgimento di soggetti quali AGID per le
+              sue funzioni istituzionali in materia di interoperabilità.
+            </p>
+            <p style={{ fontSize: 20 }}>
+              L’Istat provvede allo sviluppo del catalogo e alla sua
+              implementazione e fornisce, laddove richiesto, supporto tecnico e
+              metodologico alle pubbliche amministrazioni nella definizione di
+              ontologie, vocabolari controllati e schemi di dati relativi ai
+              propri dataset. La responsabilità della scelta e della
+              pubblicazione di un determinato asset semantico è della pubblica
+              amministrazione che provvede alla pubblicazione sul proprio
+              repository; la validazione rimane, quindi, di competenza della
+              stessa pubblica amministrazione.
+            </p>
+            <p style={{ fontSize: 20 }}>
+              La titolarità dei contenuti semantici pubblicati nel catalogo è,
+              quindi, degli enti titolari delle basi di dati cui i singoli
+              contenuti afferiscono, ferma restando la corretta attribuzione di
+              paternità degli stessi.
+            </p>
+            <div className="text-center text-sm-left pb-4 display-2">
+              Licenza dei contenuti
+            </div>
+            <p style={{ fontSize: 20 }}>
+              Ove non diversamente specificato, i contenuti pubblicati su
+              schema.gov.it sono rilasciati dalla Presidenza del Consiglio dei
+              Ministri – Dipartimento per la Trasformazione Digitale con{" "}
+              <Anchor href="https://creativecommons.org/licenses/by/4.0/legalcode.it">
+                licenza CC-BY 4.0
+              </Anchor>{" "}
+              . Gli utenti possono, nel rispetto delle disposizioni vigenti,
+              condividere (riprodurre, distribuire, comunicare al pubblico,
+              esporre in pubblico, rappresentare, eseguire e recitare questo
+              materiale con qualsiasi mezzo e formato) e modificare (trasformare
+              il materiale e utilizzarlo per opere derivate) per qualsiasi fine,
+              anche commerciale con il solo onere di attribuzione, senza apporre
+              restrizioni aggiuntive.{" "}
+            </p>
+            <p style={{ fontSize: 20 }}>
+              Per la corretta attribuzione di paternità da parte
+              dell’utilizzatore, si rinvia all’individuazione del “Creatore”
+              indicato per ciascuna risorsa del catalogo.
+            </p>
+
+            <div className="text-center text-sm-left pb-4 display-2">
+              Collegamenti a siti esterni
+            </div>
+            <p style={{ fontSize: 20 }}>
+              I collegamenti a siti esterni di terzi, indicati in questo sito,
+              nonché i contenuti incorporati di terzi sono forniti come servizio
+              agli utenti. L’indicazione dei collegamenti a siti esterni di
+              terzi o l’inserimento di contenuti incorporati di terzi non
+              implica alcun tipo di approvazione o condivisione di
+              responsabilità in relazione alla legittimità, alla completezza e
+              alla correttezza delle informazioni contenute nei siti indicati.
+            </p>
           </div>
         </div>
       </div>
