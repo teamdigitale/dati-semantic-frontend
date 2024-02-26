90575a3f10255c9b5a31858673f79024132e4ce5
VincenzoDeSisto <160047529+VincenzoDeSisto@users.noreply.github.com>
2024-02-26 10:05:49 +0100

    fix(unit-test): fix errors in unit tests (#489)
    * fix(general): fix all prettier problems
    
    * fix(details-responsive): fix responsive background height in asset details page
    
    Refs #474
    
    * fix(footer-ui): fix alignment logos and remove governo logo
    
    Refs #485
    
    * fix(schema-ui): fix responsive of concetti semantici in schema metadata
    
    * fix(general): fix background for all details and fix breadcrumbs
    
    * fix(unit-test): fix errors in unit tests

--- src/components/common/BreadCrumbs/BreadCrumbs.test.js
+++ src/components/common/BreadCrumbs/BreadCrumbs.test.js
@@ -1,19 +1,22 @@
-import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
-import BREADCRUMBS from "../../../services/BreadCrumbsConst";
-import BreadCrumbs from "./BreadCrumbs";
+import { screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { waitFor } from "@testing-library/dom";
+
+import BreadCrumbs from "./BreadCrumbs";
+import BREADCRUMBS from "../../../services/BreadCrumbsConst";
+import { renderWithRoute } from "../../../services/testUtils";
+
 describe("<BreadCrumbs />", () => {
   test("it should mount", async () => {
-    render(<BreadCrumbs arrayBread={BREADCRUMBS.EXPLOREPAGE} />);
+    renderWithRoute(<BreadCrumbs arrayBread={BREADCRUMBS.EXPLOREPAGE} />);
 
     const link = screen.getByText("Home");
     expect(link).toBeInTheDocument();
     userEvent.click(link);
   });
   test("it should navigate", async () => {
-    render(<BreadCrumbs arrayBread={BREADCRUMBS.EXPLOREPAGE} />);
+    renderWithRoute(<BreadCrumbs arrayBread={BREADCRUMBS.EXPLOREPAGE} />);
     global.window = { location: { pathname: null } };
     const link = screen.getByText("Home");
     userEvent.click(link);
