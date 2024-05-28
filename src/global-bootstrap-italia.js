63e9c8b916d83c92b0eb4918e14e8e849d28be88
VincenzoDeSisto <vincenzo.desisto@innovaway.it>
2024-05-28 10:24:41 +0200

    chore(libraries): fix error modal bootstrap

--- src/global-bootstrap-italia.js
+++ src/global-bootstrap-italia.js
@@ -1,8 +1,10 @@
 import jquery from "jquery";
 import Popper from "popper.js";
+import Modal from "bootstrap/js/dist/modal";
 
 window.__PUBLIC_PATH__ = "/fonts";
 window.Popper = Popper;
 window.jQuery = jquery;
 window.jquery = jquery;
+window.Modal = Modal;
 window.$ = jquery;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import "./styles/index.css";
 import App from "./App";
 import { loadFonts } from "bootstrap-italia";
 
+// import "bootstrap-italia/dist/css/bootstrap-italia.min.css";
+// import "bootstrap-italia/dist/js/bootstrap-italia.min.js";
+
 loadFonts("/bootstrap-italia/dist/fonts");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
